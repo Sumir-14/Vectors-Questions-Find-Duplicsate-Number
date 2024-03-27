864a6524832bbcb89929a6ea19daced5e423a6c9
Sumir-14 <165183364+Sumir-14@users.noreply.github.com>
2024-03-27 16:40:48 +0000

    added comment

--- .ccls-cache/@home@runner@Vectors-Questions-Find-Duplicsate-Number/main.cpp
+++ .ccls-cache/@home@runner@Vectors-Questions-Find-Duplicsate-Number/main.cpp
@@ -1,27 +1,20 @@
+#include <algorithm>
 #include <iostream>
-#include<algorithm>
-#include<vector>
+#include <vector>
 using namespace std;
-void duplicatenum(vector<int>& num) {
-  sort(num.begin(), num.end());
-}
+void duplicatenum(vector<int> &num) { sort(num.begin(), num.end()); }
 
+// int main()
 int main() {
-  vector<int> num = {2,3,1,3,5};
-    duplicatenum(num);
+  vector<int> num = {2, 3, 1, 3, 5};
+  duplicatenum(num);
 
-  for (int i=0;i<num.size()-1;i++)
-    {
-      if(num[i]==num[i+1])
-      {
-        
-        cout<<num[i]<<" ";
-      }
-      else
-      {
-        return 0;
-      }
+  for (int i = 0; i < num.size() - 1; i++) {
+    if (num[i] == num[i + 1]) {
+
+      cout << num[i] << " ";
+    } else {
+      return 0;
     }
-  
-
+  }
 }
