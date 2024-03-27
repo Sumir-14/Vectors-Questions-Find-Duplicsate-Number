dbbc90824bc81d8e4138d4a4da2f573dc3a117d6
Sumir-14 <165183364+Sumir-14@users.noreply.github.com>
2024-03-27 16:34:14 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Vectors-Questions-Find-Duplicsate-Number/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+void duplicatenum(vector<int>& num) {
+  sort(num.begin(), num.end());
+}
+
+int main() {
+  vector<int> num = {2,3,1,3,5};
+    duplicatenum(num);
+
+  for (int i=0;i<num.size()-1;i++)
+    {
+      if(num[i]==num[i+1])
+      {
+        
+        cout<<num[i]<<" ";
+      }
+      else
+      {
+        return 0;
+      }
+    }
+  
+
+}
